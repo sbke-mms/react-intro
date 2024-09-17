1d432e56ce06d741d128c16ad6d8407cf148848c
Sebastian Kiehne <sebastian.kiehne@telekom.de>
2024-09-17 11:09:14 +0200

    only use children prop

--- tag1/src/layouts/MainLayout.jsx
+++ tag1/src/layouts/MainLayout.jsx
@@ -1,11 +1,11 @@
 import Footer from "../components/Footer";
 import Header from "../components/Header/Header";
 
-export default function MainLayout (props) {
+export default function MainLayout ({children}) {
     return <>
         <Header/>
         <main>
-            {props.children}
+            {children}
         </main>
         <Footer/>
     </>
