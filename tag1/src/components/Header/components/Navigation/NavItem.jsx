1055bdd98a34770c3e76b8a924c51eec4257b2ac
Sebastian Kiehne <sebastian.kiehne@telekom.de>
2024-09-17 13:07:27 +0200

    state management for components

--- tag1/src/components/Header/components/Navigation/NavItem.jsx
+++ tag1/src/components/Header/components/Navigation/NavItem.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
+import { useState } from 'react'
 
 function NavItem({title, href}) {
-  return <li>
-        <a href={href}>
+    const [change, setChange] = useState(false)
+    function changeColor() {
+        setChange(!change) // only active after rendering
+        //console.log("in function:", change)
+    }
+
+    //console.log("outer function:", change)
+    return <li onClick={changeColor} style={{color:change?"green":"red"}} >
             {title}
-        </a>
     </li>
 }
 
